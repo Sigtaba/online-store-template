2ac4592bb81779f7a74e64473290f5b500585bd6
Isabella Abatgis <abatgis@gmail.com>
2017-07-11 21:42:40 -0700

    add firebase database

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -1,8 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Album } from '../album.model';
 import { AlbumService } from '../album.service';
+import { FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-album-detail',
@@ -12,21 +14,16 @@ import { AlbumService } from '../album.service';
 })
 
 export class AlbumDetailComponent implements OnInit {
-  albumId: number;
-  albumToDisplay: Album;
+  albumId: string;
+  albumToDisplay;
 
-  constructor(
-    private route: ActivatedRoute,
-    private location: Location,
-    private albumService: AlbumService
-  ) {}
+  constructor(private route: ActivatedRoute, private location: Location, private albumService: AlbumService) { }
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
-     this.albumId = parseInt(urlParameters['id']);
+     this.albumId = urlParameters['id'];
    });
    this.albumToDisplay = this.albumService.getAlbumById(this.albumId);
   }
-
 
 }
