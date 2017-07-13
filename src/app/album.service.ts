dcd9b118dcc6bd710911dfbea3b12f90401df20f
Isabella Abatgis <abatgis@gmail.com>
2017-07-12 19:51:50 -0700

    add edit functionality

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -19,7 +19,14 @@ export class AlbumService {
   }
 
   getAlbumById(albumId: string){
-    return this.database.object('albums/' + albumId);
+    return this.database.object('/albums/' + albumId);
+  }
+
+  updateAlbum(localUpdatedAlbum){
+    var albumEntryInFirebase = this.getAlbumById(localUpdatedAlbum.$key);
+    albumEntryInFirebase.update({title: localUpdatedAlbum.title,
+                                artist: localUpdatedAlbum.artist,
+                                description: localUpdatedAlbum.description});
   }
 
 }
